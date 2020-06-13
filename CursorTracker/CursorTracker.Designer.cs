1ec358ec70828d6cbb824647c720f5b1551e6106
Oskar Mroz <47431553+MoreeZ@users.noreply.github.com>
2020-06-13 14:33:22 +0100

    added color feature

--- CursorTracker/CursorTracker.Designer.cs
+++ CursorTracker/CursorTracker.Designer.cs
@@ -32,6 +32,13 @@
             this.y = new System.Windows.Forms.Label();
             this.lbly = new System.Windows.Forms.Label();
             this.lblx = new System.Windows.Forms.Label();
+            this.redlbl = new System.Windows.Forms.Label();
+            this.greenlbl = new System.Windows.Forms.Label();
+            this.bluelbl = new System.Windows.Forms.Label();
+            this.redval = new System.Windows.Forms.Label();
+            this.greenval = new System.Windows.Forms.Label();
+            this.blueval = new System.Windows.Forms.Label();
+            this.colordisplay = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // x
@@ -56,35 +63,111 @@
             // 
             // lbly
             // 
-            this.lbly.AutoSize = true;
+            this.lbly.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lbly.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbly.Location = new System.Drawing.Point(63, 53);
+            this.lbly.Location = new System.Drawing.Point(61, 53);
             this.lbly.Name = "lbly";
-            this.lbly.Size = new System.Drawing.Size(104, 44);
+            this.lbly.Size = new System.Drawing.Size(107, 44);
             this.lbly.TabIndex = 2;
             this.lbly.Text = "0000";
+            this.lbly.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblx
             // 
-            this.lblx.AutoSize = true;
+            this.lblx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblx.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblx.Location = new System.Drawing.Point(63, 9);
+            this.lblx.Location = new System.Drawing.Point(61, 9);
             this.lblx.Name = "lblx";
-            this.lblx.Size = new System.Drawing.Size(104, 44);
+            this.lblx.Size = new System.Drawing.Size(107, 44);
             this.lblx.TabIndex = 3;
             this.lblx.Text = "0000";
+            this.lblx.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // redlbl
+            // 
+            this.redlbl.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redlbl.Location = new System.Drawing.Point(12, 138);
+            this.redlbl.Name = "redlbl";
+            this.redlbl.Size = new System.Drawing.Size(96, 28);
+            this.redlbl.TabIndex = 5;
+            this.redlbl.Text = "Red:";
+            // 
+            // greenlbl
+            // 
+            this.greenlbl.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenlbl.Location = new System.Drawing.Point(12, 166);
+            this.greenlbl.Name = "greenlbl";
+            this.greenlbl.Size = new System.Drawing.Size(96, 28);
+            this.greenlbl.TabIndex = 6;
+            this.greenlbl.Text = "Green: ";
+            // 
+            // bluelbl
+            // 
+            this.bluelbl.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bluelbl.Location = new System.Drawing.Point(12, 194);
+            this.bluelbl.Name = "bluelbl";
+            this.bluelbl.Size = new System.Drawing.Size(96, 28);
+            this.bluelbl.TabIndex = 7;
+            this.bluelbl.Text = "Blue: ";
+            // 
+            // redval
+            // 
+            this.redval.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redval.Location = new System.Drawing.Point(114, 138);
+            this.redval.Name = "redval";
+            this.redval.Size = new System.Drawing.Size(96, 28);
+            this.redval.TabIndex = 8;
+            this.redval.Text = "000";
+            // 
+            // greenval
+            // 
+            this.greenval.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenval.Location = new System.Drawing.Point(114, 166);
+            this.greenval.Name = "greenval";
+            this.greenval.Size = new System.Drawing.Size(96, 28);
+            this.greenval.TabIndex = 9;
+            this.greenval.Text = "000";
+            // 
+            // blueval
+            // 
+            this.blueval.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueval.Location = new System.Drawing.Point(114, 194);
+            this.blueval.Name = "blueval";
+            this.blueval.Size = new System.Drawing.Size(96, 28);
+            this.blueval.TabIndex = 10;
+            this.blueval.Text = "000";
+            // 
+            // colordisplay
+            // 
+            this.colordisplay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.colordisplay.BackColor = System.Drawing.Color.Red;
+            this.colordisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.colordisplay.Location = new System.Drawing.Point(17, 106);
+            this.colordisplay.Name = "colordisplay";
+            this.colordisplay.Size = new System.Drawing.Size(151, 22);
+            this.colordisplay.TabIndex = 11;
             // 
             // CursorTracker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(186, 109);
+            this.ClientSize = new System.Drawing.Size(180, 228);
+            this.Controls.Add(this.colordisplay);
+            this.Controls.Add(this.blueval);
+            this.Controls.Add(this.greenval);
+            this.Controls.Add(this.redval);
+            this.Controls.Add(this.bluelbl);
+            this.Controls.Add(this.greenlbl);
+            this.Controls.Add(this.redlbl);
             this.Controls.Add(this.lblx);
             this.Controls.Add(this.lbly);
             this.Controls.Add(this.y);
             this.Controls.Add(this.x);
-            this.MaximumSize = new System.Drawing.Size(202, 148);
-            this.MinimumSize = new System.Drawing.Size(202, 148);
+            this.MaximumSize = new System.Drawing.Size(196, 267);
+            this.MinimumSize = new System.Drawing.Size(196, 267);
             this.Name = "CursorTracker";
             this.RightToLeftLayout = true;
             this.Text = "CursorTracker";
@@ -97,7 +180,13 @@
         private System.Windows.Forms.Label y;
         private System.Windows.Forms.Label lbly;
         private System.Windows.Forms.Label lblx;
-
+        private System.Windows.Forms.Label redlbl;
+        private System.Windows.Forms.Label greenlbl;
+        private System.Windows.Forms.Label bluelbl;
+        private System.Windows.Forms.Label redval;
+        private System.Windows.Forms.Label greenval;
+        private System.Windows.Forms.Label blueval;
+        private System.Windows.Forms.Label colordisplay;
     }
     
 }
--- CursorTracker/CursorTracker.cs
+++ CursorTracker/CursorTracker.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using MouseKeyboardActivityMonitor;
 using MouseKeyboardActivityMonitor.WinApi;
@@ -7,27 +11,81 @@ namespace CursorTracker
 {
     public partial class CursorTracker : Form
     {
-        private MouseHookListener m_mouseListener;
+        private readonly MouseHookListener m_mouseListener;
+        private readonly KeyboardHookListener keyboardListener;
+
+
         public CursorTracker()
         {
             InitializeComponent();
+
             //this.MouseUp += new MouseButtonEventHandler(OnMouseup); 
             // Note: for an application hook, use the AppHooker class instead
-            m_mouseListener = new MouseHookListener(new GlobalHooker());
-
-            // The listener is not enabled by default
-            m_mouseListener.Enabled = true;
-
+            m_mouseListener = new MouseHookListener(new GlobalHooker())
+            {
+                // The listener is not enabled by default
+                Enabled = true
+            };
             m_mouseListener.MouseMoveExt += MyMouseMoveExt;
+
+            keyboardListener = new KeyboardHookListener(new GlobalHooker())
+            {
+                Enabled = true
+            };
+            keyboardListener.KeyPress += DetectMinusAndPause;
         }
 
-        
+
 
         private void MyMouseMoveExt(object sender, MouseEventExtArgs e)
         {
             lblx.Text = e.X.ToString();
             lbly.Text = e.Y.ToString();
+
+            //Point cursor = new Point();
+            var c = GetColorAt(e.Location);
+            colordisplay.BackColor = Color.FromArgb(c.R, c.G, c.B);
+            redval.Text = c.R.ToString();
+            greenval.Text = c.G.ToString();
+            blueval.Text = c.B.ToString();
+            //Console.WriteLine($"RED: {c.R}, GREEN: {c.G}, BLUE: {c.B}");
+
         }
 
+        private void DetectMinusAndPause(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '-')
+            {
+                if (m_mouseListener.Enabled)
+                {
+                    m_mouseListener.Enabled = false;
+                }else
+                {
+                    m_mouseListener.Enabled = true;
+                }
+            }
+        }
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        readonly Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
     }
 }
